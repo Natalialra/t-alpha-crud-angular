8d79dc6e6b05a79b2adae3fd8b6d8860ebb11e55
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-09 13:47:43 -0300

    feat-criacao-form-register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public registerForm: FormGroup = this.initializeForm();
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  // tslint:disable-next-line:typedef
+  initializeForm() {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      taxNumber: ['', Validators.required],
+      mail: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    });
+  }
+
+  get name () {
+    return this.registerForm.get('name');
+  }
+
+  get taxNumber () {
+    return this.registerForm.get('taxNumber');
+  }
+
+  get mail () {
+    return this.registerForm.get('mail');
+  }
+
+  get phone () {
+    return this.registerForm.get('phone');
+  }
+
+  get password () {
+    return this.registerForm.get('password');
+  }
+
+  get confirmPassword () {
+    return this.registerForm.get('confirmPassword');
+  }
+
 
 }
