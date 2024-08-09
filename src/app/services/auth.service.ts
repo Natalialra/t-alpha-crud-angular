d548089f62d2ac6891e1d499ce7c6c0458b8125d
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-09 16:48:59 -0300

    feat-atualizacao-funcao-register-no-service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {User} from '../models/user';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,8 @@ import {HttpClient} from '@angular/common/http';
 export class AuthService {
   private apiUrl = 'https://interview.t-alpha.com.br/api/auth';
   constructor(private http: HttpClient) { }
+
+  register(user: User): Observable<any>{
+    return this.http.post(`${this.apiUrl}/register`, user);
+  }
 }
