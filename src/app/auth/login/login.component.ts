c49779ab9652f831254ee6783e95494ef2e839e4
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 09:16:48 -0300

    feat-criacao-funcao-login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +13,12 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup = this.initializeForm();
 
-  constructor(private formBuilder: FormBuilder) {
-  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -31,7 +38,27 @@ export class LoginComponent implements OnInit {
     return this.loginForm.get('password');
   }
 
-  login(): void {
-    // TODO
+  async login(loginForm: FormGroup): Promise<void> {
+    if (loginForm.valid) {
+      const credentials = this.loginForm.value;
+
+      try {
+        const response = await this.authService.login(credentials).toPromise();
+
+        if (response.success) {
+          console.log('Successfully logged in');
+          const token = response.data.token;
+          localStorage.setItem('authToken', token);
+          this.authService.setAuthenticationHeaders(token);
+
+          this.toastr.success('Login realizado com sucesso!');
+          await this.router.navigate(['/products']);
+        } else {
+          this.toastr.error('Erro ao realizar login, verifique os dados informados');
+        }
+      } catch (error) {
+        console.error('Erro ao realizar login:', error);
+      }
+    }
   }
 }
