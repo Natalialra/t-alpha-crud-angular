f7bfedc2acaa9f4df425f420838408a700981203
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 09:53:54 -0300

    feat-atualizacao-service-produtos-create

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import { Products} from '../models/products';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private apiUrl = 'https://interview.t-alpha.com.br/api/products';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  createProduct(product: Products): Observable<any>{
+    return this.http.post(`${this.apiUrl}/create-product`, product);
+  }
 }
