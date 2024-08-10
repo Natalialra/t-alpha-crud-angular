1d87732e8a10c36067befc9f269b9c604525e15a
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 20:03:18 -0300

    feat-atualizacao-chamada-para-edicao

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output} from '@angular/core';
 import {ProductsService} from '../../services/products.service';
 import {Products} from '../../models/products';
 import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-products-list',
@@ -9,10 +10,18 @@ import {Observable} from 'rxjs';
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
-public products$: Observable<Products[]> = this.productsService.getAllProducts() as Observable<Products[]>;
-  constructor(private productsService: ProductsService) { }
+  public products$: Observable<Products[]> = this.productsService.getAllProducts() as Observable<Products[]>;
+
+  constructor(
+    private productsService: ProductsService,
+    private router: Router) {
+  }
+
   ngOnInit(): void {
-    this.products$.subscribe(products => console.log(products));
+  }
+
+  edit(id): void {
+    this.router.navigate([`newproduct/${id}`]);
   }
 
 }
