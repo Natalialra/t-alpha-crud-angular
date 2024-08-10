6d4d62d681ad1da9e4c187665bafcb3dffceea2e
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 11:04:52 -0300

    feat-atualizacao-rota-de-produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
+import {ProductsListComponent} from './products/products-list/products-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'products', component: ProductsListComponent },
 ];
 
 @NgModule({
