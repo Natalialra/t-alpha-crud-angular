7e09914d025efa01b4c317161f7894179e5d0982
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 16:59:16 -0300

    feat-inclusao-form-productsform

--- src/app/products/products-form/products-form.component.ts
+++ src/app/products/products-form/products-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-products-form',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-form.component.css']
 })
 export class ProductsFormComponent implements OnInit {
+  public productForm: FormGroup = this.initializeForm();
+  public isEditMode = false;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  initializeForm(): FormGroup {
+    return this.fb.group({
+      id: [null],
+      name: ['', Validators.required],
+      description: [''],
+      price: ['', [Validators.required, Validators.min(0)]],
+      stock: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  get name(): AbstractControl {
+    return this.productForm.get('name');
+  }
+
+  get description(): AbstractControl {
+    return this.productForm.get('description');
+  }
+
+  get price(): AbstractControl {
+    return this.productForm.get('price');
+  }
+
+  get stock(): AbstractControl {
+    return this.productForm.get('stock');
+  }
+
+  createProduct(): void {
+   return null;
+  }
 }
