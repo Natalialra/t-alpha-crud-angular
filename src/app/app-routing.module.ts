5387294969b41115ab2b80de5a542abd7617cc36
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 18:42:02 -0300

    feat-atualizacao-criacao-produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {ProductsListComponent} from './products/products-list/products-list.component';
+import {ProductsFormComponent} from './products/products-form/products-form.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'products', component: ProductsListComponent },
+  { path: '', component: ProductsListComponent },
+  { path: 'newproduct', component: ProductsFormComponent },
+  { path: 'products', component: ProductsListComponent}
 ];
 
 @NgModule({
