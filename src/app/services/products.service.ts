64fbe35bb2b82e093dd1cf67480bd44e237c53f5
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 12:17:44 -0300

    feat-inclusao-token-nas-requisicoes

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 import { Products} from '../models/products';
 import {Observable} from 'rxjs';
@@ -9,21 +9,26 @@ import {Observable} from 'rxjs';
 })
 export class ProductsService {
   private apiUrl = 'https://interview.t-alpha.com.br/api/products';
+  public headers = this.createAuthHeaders();
   constructor(private http: HttpClient) { }
 
+  private createAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
   createProduct(product: Products): Observable<any>{
-    return this.http.post(`${this.apiUrl}/create-product`, product);
+    return this.http.post(`${this.apiUrl}/create-product`, product, {headers: this.headers});
   }
   getAllProducts(): Observable<any>{
-    return this.http.get(`${this.apiUrl}/get-all-products`);
+    return this.http.get(`${this.apiUrl}/get-all-products`, {headers: this.headers});
   }
   getProductById(productId: number): Observable<any>{
-    return this.http.get(`${this.apiUrl}/get-one-product/${productId}`);
+    return this.http.get(`${this.apiUrl}/get-one-product/${productId}`, {headers: this.headers});
   }
   updateProduct(productId: number, product: Products): Observable<any>{
-    return this.http.patch(`${this.apiUrl}/update-product/${productId}`, product);
+    return this.http.patch(`${this.apiUrl}/update-product/${productId}`, product,{headers: this.headers});
   }
   deleteProduct(productId: number): Observable<any>{
-    return this.http.delete(`${this.apiUrl}/delete-product/${productId}`);
+    return this.http.delete(`${this.apiUrl}/delete-product/${productId}`, {headers: this.headers});
   }
 }
