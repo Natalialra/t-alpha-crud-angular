d49cb200bdf9ee6c39673b1863575c16d3097525
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 08:29:54 -0300

    feat-criacao-login-form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup = this.initializeForm();
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit(): void {
   }
 
+  initializeForm(): any {
+    return this.formBuilder.group({
+      taxNumber: ['', [Validators.required, Validators.minLength(11)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get taxNumber(): any {
+    return this.loginForm.get('taxNumber');
+  }
+
+  get password(): any {
+    return this.loginForm.get('password');
+  }
+
+  login(): void {
+    // TODO
+  }
 }
