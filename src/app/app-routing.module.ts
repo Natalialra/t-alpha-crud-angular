1da42f9d69905b7c21256ad827c4ebf05109921c
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 20:08:38 -0300

    feat-atualizacao-nome-da-rota-produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {ProductsListComponent} from './products/products-list/products-list.component';
-import {ProductsFormComponent} from './products/products-form/products-form.component';
+import {ProductsFormComponent} from './products/products-list/products-form/products-form.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', component: ProductsListComponent },
-  { path: 'newproduct', component: ProductsFormComponent },
+  { path: 'product', component: ProductsFormComponent },
+  { path: 'product/:id', component: ProductsFormComponent },
   { path: 'products', component: ProductsListComponent}
 ];
 
