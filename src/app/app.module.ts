8b9eaa487a21e9e2437596e2ca741f18b17ec4d3
Natalia <natalia.ribeiro@groupsoftware.com.br>
2024-08-10 20:11:46 -0300

    fix-exclusao-iconfig-sem-uso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { ProductsListComponent } from './products/products-list/products-list.component';
 import { AuthInterceptorService } from './services/auth-interceptor.service';
 import { ProductsFormComponent } from './products/products-list/products-form/products-form.component';
-import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { NgxMaskModule } from 'ngx-mask';
 
 
 @NgModule({
